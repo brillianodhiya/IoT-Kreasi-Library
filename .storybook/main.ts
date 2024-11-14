df347a014325564fa2cc4e209ff1a472e1f4bf37
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-11-14 18:02:17 +0700

    add addon in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,9 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-storysource", // View a story’s source code to see how it works and paste into your app https://storybook.js.org/addons/@storybook/addon-storysource
+    "@storybook/addon-designs", // Storybook addon for embedding your design preview in addon panel https://storybook.js.org/addons/@storybook/addon-designs
+    "@storybook/addon-viewport", // Build responsive components by adjusting Storybook’s viewport size and orientation https://storybook.js.org/addons/@storybook/addon-viewport
   ],
   framework: {
     name: "@storybook/react-vite",
