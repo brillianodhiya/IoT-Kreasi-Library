a2c56188d80f5656d70ed8beea71af15a3d8d6d1
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-11-14 15:35:09 +0700

    remove dist from github

--- dist/components/Example/ExampleComponent.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-type Props = {
-    onClick?: () => void;
-    style?: React.CSSProperties;
-    type?: "button" | "submit" | "reset";
-    children?: React.ReactNode;
-};
-declare const ExampleComponent: ({ type, onClick, style, children, }: Props) => React.JSX.Element;
-export default ExampleComponent;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExampleComponent = void 0;
-var ExampleComponent_1 = require("./components/Example/ExampleComponent");
-Object.defineProperty(exports, "ExampleComponent", { enumerable: true, get: function () { return __importDefault(ExampleComponent_1).default; } });
