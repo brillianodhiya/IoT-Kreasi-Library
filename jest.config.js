f5ef6f0ffeb59c85fe717f6fba02830b5594d221
Brilliano Dhiya Ulhaq <brillidhiya@gmail.com>
2024-11-19 09:02:59 +0700

    update: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
-  snapshotSerializers: [
-    "@emotion/jest/serializer" /* if needed other snapshotSerializers should go here */,
-  ],
 };
